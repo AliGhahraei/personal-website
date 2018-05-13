6deb4628b0be8863841ffaa8a3631c4eaf1a5a41
Ali Ghahraei <aligf94@gmail.com>
2018-05-13 12:54:35 -0500

    Revert "Don't delete the output directory"
    This reverts commit c72bf075fe5505aa37ee30762cc6fdd53cbbdb5f.

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ from pelicanconf import *
 SITEURL = ''
 RELATIVE_URLS = False
 
-#DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = True
 
 #FEED_ALL_ATOM = 'feeds/all.atom.xml'
 #CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'

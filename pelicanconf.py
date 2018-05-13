949677a630e24e7abe7804917d16cad95be3b1e3
Ali Ghahraei <aligf94@gmail.com>
2018-05-13 12:59:01 -0500

    Disable output directory deletion in dev environment

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,6 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-DELETE_OUTPUT_DIRECTORY = True
 IGNORE_FILES = ['.#*', '__pycache__']
 USER_THEME_PATH = expanduser(join('~', 'g'))
 THEME_NAME = 'attila'

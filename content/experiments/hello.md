25d83fb22df58d647d1f2663c3a7952ed719c267
Ali Ghahraei <aligf94@gmail.com>
2018-05-13 14:18:55 -0500

    Move to org for readme and content

--- content/experiments/hello.md
+++ content/experiments/hello.md
@@ -1,3 +1,4 @@
 Title: Hello Pelican
 Date: 2018-03-20 08:47
 
+This is a test article using Markdown. Hello, Pelican!
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*- #
 #pylint: disable=missing-docstring
 from __future__ import unicode_literals
+from platform import system
 from os.path import expanduser, join
+
+SYSTEM = system()
 
 AUTHOR = 'Ali Ghahraei Figueroa'
 SITENAME = 'Ali Ghahraei Figueroa'
@@ -34,8 +37,14 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
 IGNORE_FILES = ['.#*', '__pycache__']
+PLUGINS = ['org_reader']
+
+
 USER_THEME_PATH = expanduser(join('~', 'g'))
 THEME_NAME = 'attila'
 THEME = join(USER_THEME_PATH, THEME_NAME)
+
+PLUGIN_PATHS = [expanduser(join('~', 'g', 'pelican-plugins'))]
+ORG_READER_EMACS_LOCATION = ('/Applications/Emacs.app/Contents/MacOS/Emacs' if SYSTEM == 'Darwin'
+                             else '/usr/bin/emacs')

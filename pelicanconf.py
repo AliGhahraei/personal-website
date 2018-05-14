22deae3a2e9e12cc173034fde79abc385ba52a6f
Ali Ghahraei <aligf94@gmail.com>
2018-05-14 10:14:40 -0500

    Include README.org in published site

--- pelicanconf.py
+++ pelicanconf.py
@@ -41,7 +41,6 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 IGNORE_FILES = ['.#*', '__pycache__']
-STATIC_PATHS = ['README.org']
 PLUGINS = ['org_reader']
 
 

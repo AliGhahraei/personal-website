c57a124a0a73177b7d4c730083aafa61236e483b
Ali Ghahraei <aligf94@gmail.com>
2018-05-14 09:26:56 -0500

    Change theme and add custom export settings

---
+++ export-settings.el
@@ -0,0 +1,1 @@
+(setq org-export-with-section-numbers nil)
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 #pylint: disable=missing-docstring
 from __future__ import unicode_literals
 from platform import system
-from os.path import expanduser, join
+from os.path import expanduser, join, dirname
 
 SYSTEM = system()
 
@@ -16,6 +16,9 @@ PATH = 'content'
 TIMEZONE = 'America/Monterrey'
 
 DEFAULT_LANG = 'en'
+
+USE_FOLDER_AS_CATEGORY = True
+DISPLAY_CATEGORIES_ON_MENU = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -38,13 +41,15 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 IGNORE_FILES = ['.#*', '__pycache__']
+STATIC_PATHS = ['README.org']
 PLUGINS = ['org_reader']
 
 
-USER_THEME_PATH = expanduser(join('~', 'g'))
-THEME_NAME = 'attila'
+USER_THEME_PATH = expanduser(join('~', 'g', 'pelican-themes'))
+THEME_NAME = 'nest'
 THEME = join(USER_THEME_PATH, THEME_NAME)
 
 PLUGIN_PATHS = [expanduser(join('~', 'g', 'pelican-plugins'))]
 ORG_READER_EMACS_LOCATION = ('/Applications/Emacs.app/Contents/MacOS/Emacs' if SYSTEM == 'Darwin'
                              else '/usr/bin/emacs')
+ORG_READER_EMACS_SETTINGS = join(dirname(__file__), 'export-settings.el')

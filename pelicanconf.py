a3cc5af4c0cb6bf7d73927712c6b80b88fb26fdd
Ali Ghahraei <aligf94@gmail.com>
2018-05-26 11:23:12 -0500

    Remove a propaganda link as pelican already has it

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,9 +29,10 @@ AUTHOR_FEED_RSS = None
 
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),)
+LINKS = (
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
 
 
 # Social widget

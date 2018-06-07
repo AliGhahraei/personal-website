120bfe567f55cc0e5a2316497541da69f1752cae
Ali Ghahraei <aligf94@gmail.com>
2018-06-07 03:44:17 -0500

    Update CI file and fix script's import and definition order

--- clone_or_pull_pelican_dirs.py
+++ clone_or_pull_pelican_dirs.py
@@ -4,21 +4,12 @@ from os.path import dirname
 
 from sh import cd, git, mkdir, rm
 
-from .publishconf import THEME_NAME, THEME_REMOTE
+from publishconf import THEME_NAME, THEME_REMOTE
 
 
 ORIGINAL_DIR = dirname(__file__)
 THEMES_PATH = environ["PELICAN_THEMES_PATH"]
 PLUGINS_PATH = environ["PELICAN_PLUGINS_PATH"]
-
-
-mkdir('-p', THEMES_PATH)
-cd(THEMES_PATH)
-clone_or_pull(THEME_NAME, THEME_REMOTE, rm_on_clone=True)
-cd(ORIGINAL_DIR)
-
-clone_or_pull(PLUGINS_PATH, 'https://github.com/getpelican/pelican-plugins/')
-cd(ORIGINAL_DIR)
 
 
 def clone_or_pull(repo_name, url, rm_on_clone=False):
@@ -30,3 +21,12 @@ def clone_or_pull(repo_name, url, rm_on_clone=False):
         git('clone', url, repo_name)
     else:
         git('pull')
+
+
+mkdir('-p', THEMES_PATH)
+cd(THEMES_PATH)
+clone_or_pull(THEME_NAME, THEME_REMOTE, rm_on_clone=True)
+cd(ORIGINAL_DIR)
+
+clone_or_pull(PLUGINS_PATH, 'https://github.com/getpelican/pelican-plugins/')
+cd(ORIGINAL_DIR)
